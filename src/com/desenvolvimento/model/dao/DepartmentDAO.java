5c1f964eb701ce99f2b0e2fb28512667d7eeeac0
Mario Henrique <mariomapedrao@gmail.com>
2023-11-27 17:16:35 -0300

    Implementando interface DAO

--- src/com/desenvolvimento/model/dao/DepartmentDAO.java
+++ src/com/desenvolvimento/model/dao/DepartmentDAO.java
@@ -1,2 +1,13 @@
-package com.desenvolvimento.model.dao;public interface DepartmentDAO {
+package com.desenvolvimento.model.dao;
+
+import com.desenvolvimento.model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDAO {
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
 }
